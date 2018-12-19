bc9898bdba406bf7f4cf505171ad504ea3a63576
Priya Banerjee <priya.westport@icloud.com>
2018-12-19 14:11:30 -0500

    new

--- jquery-ui-bootstrap-masterbs3/assets/js/docs.js
+++ jquery-ui-bootstrap-masterbs3/assets/js/docs.js
@@ -52,7 +52,7 @@
 
         // make code pretty
         window.prettyPrint && prettyPrint();
-        
+
         //function replace targetblank for valid w3c
         $('a.targetblank').on('click', function() {
              window.open($(this).attr('href'));
