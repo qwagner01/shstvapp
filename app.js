8f00dc1e14485ae4a863704200f6f36b16bb4ddd
Shay <sb1006067@students.westportps.org>
2018-12-21 11:27:53 -0500

    Dad helped me :P

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const GoogleSpreadsheet = require('google-spreadsheet');
+const creds = require('./client_secret.json');
+var doc = new GoogleSpreadsheet('1bnsajR4ySnuXZLLRkoVroenfnf6ULfa-6_9H2t0yD0Q');
+var schedule = []
+
+doc.useServiceAccountAuth(creds, function(err) {
+  if (err) {
+    console.log("Auth error: ", err)
+  }
+  doc.getRows(1, function(err, rows) {
+    for (i = 0; i < rows.length; i++) {
+      schedule.push({
+        period: rows[i].period,
+        start: rows[i].start,
+        end: rows[i].end,
+        lunch: rows[i].lunch
+      })
+    }
+    // console.log(schedule);
+  })
+});
 
 var app = express();
 
 // view engine setup
+app.locals.basedir = path.join(__dirname, 'views');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -19,8 +39,52 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function (req, res) {
+  res.render('index', { title: 'SHSTVApp', schedule: schedule });
+})
+
+app.get('/schedule', function(req, res) {
+  const example = {
+    "A": [
+        { period: '1',
+          start: '07:30 A.M.',
+          end: '08:20 A.M.',
+          lunch: '0' },
+        { period: '2',
+          start: '08:25 A.M.',
+          end: '09:45 A.M.',
+          lunch: '0' },
+        { period: '3',
+          start: '09:50 A.M.',
+          end: '10:40 A.M.',
+          lunch: '0' },
+        { period: '5',
+          start: '10:45 A.M.',
+          end: '11:15 A.M.',
+          lunch: '1' },
+        { period: '5',
+          start: '11:20 A.M.',
+          end: '11:50 A.M.',
+          lunch: '2' },
+        { period: '5',
+          start: '11:55 A.M.',
+          end: '12:25 P.M.',
+          lunch: '3' },
+        { period: '8',
+          start: '12:30 P.M.',
+          end: '01:20 P.M.',
+          lunch: '0' },
+        { period: '7',
+          start: '01:25 P.M.',
+          end: '02:15 P.M.',
+          lunch: '0' }
+    ],
+    "B": [
+
+    ]
+  }
+  res.send(example)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
